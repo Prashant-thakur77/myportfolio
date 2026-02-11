8afb97dbf655a901e8b5dc7e25041ebfe8a18e25
Prashant-thakur77 <prashant101007@gmail.com>
2026-02-11 23:09:48 +0530

    done

--- src/components/nav-item-github.tsx
+++ src/components/nav-item-github.tsx
@@ -8,7 +8,6 @@ async function getStargazerCount() {
       {
         headers: {
           Accept: "application/vnd.github+json",
-          Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
           "X-GitHub-Api-Version": "2022-11-28",
         },
         next: { revalidate: 86400 }, // Cache for 1 day (86400 seconds)
